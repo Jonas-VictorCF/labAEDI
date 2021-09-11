a6ee23d31aaf48a98e1915ad6dd7d25f89e3ccde
Jonas-VictorCF <Jonas.ferreira@alunos.ufersa.edu.br>
2021-09-10 21:02:29 -0300

    Duvida.

--- Roteiro 04/Requisito02.c
+++ Roteiro 04/Requisito02.c
@@ -1,33 +1,27 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+
 int  main () {
-	
+
     FILE *arquivo = fopen ( "agenda.dat" , "r" );
 	
      if (arquivo == NULL ) {
 	
 	puts ( " Arquivo não pode ser aberto " );
 	exit ( 1 );
-	  
     }
-
    char nome[80];
-   long long int telefone;
-   int k = 0 ;
-    do {
-		fscanf (arquivo, "%s%lld" ,nome,&telefone);
-    k = k+ 1;
-    
-    if ((k % 4) == 2){
-		printf("Nome: %s\n Telefone:%lld\n ",nome,telefone);
-    }
-		
-	} while (! feof (arquivo));
+   int telefone;
+do{
+		fscanf("%s %i", nome,&telefone);
+    printf("\nNome: %s\n",nome);
+    printf("\nProfissao: %i\n",telefone);
+ }while(!feof(arquivo));
   
-	fclose (arquivo);
+	fclose(arquivo);
 		
-
+	
 	
    return ( 0 );
 }
